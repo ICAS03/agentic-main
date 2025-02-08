e17429b53ea86cf9ec316a20f3caa5b92cb5d7b6
lenovo/yeexiong17 <wyeexiong17@gmail.com>
2025-02-09 01:33:46 +0800

    fix: Fix for failed deployment on OneSec

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+    output: 'export',
     distDir: 'dist',
     images: {
         unoptimized: true,
     },
+    trailingSlash: true,
 }
 
 module.exports = nextConfig 
